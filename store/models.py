02aca1937bdc863a81818e5b2f86a37f8124c2ce
Musthak <musthakcool55@gmail.com>
2026-02-05 17:18:01 +0530

    new model added

--- store/models.py
+++ store/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("category_detail", args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
