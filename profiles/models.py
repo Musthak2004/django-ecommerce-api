04afd878ebe5f9a746c1c935fd4670cf844ccf69
Musthak <musthakcool55@gmail.com>
2026-02-09 21:04:22 +0530

    profile page created

--- profiles/models.py
+++ profiles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.urls import reverse
-
+from django.utils.text import slugify
 
 class Profile(models.Model):
     user = models.OneToOneField(
@@ -9,6 +9,7 @@ class Profile(models.Model):
         on_delete=models.CASCADE,
         related_name="profile"
     )
+    slug = models.SlugField(unique=True, blank=True)
     bio = models.TextField(blank=True)
     profile_pic = models.ImageField(upload_to="profiles/", blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
@@ -28,4 +29,9 @@ class Profile(models.Model):
         return f"{self.user} Profile"
 
     def get_absolute_url(self):
-        return reverse("profile_detail", kwargs={"pk": self.pk})
+        return reverse("profile_detail", kwargs={"slug": self.slug})
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.user.username)
+        super().save(*args, **kwargs)
