b95b581ea1331de6f424c553dcbf38eec98798bc
Musthak <musthakcool55@gmail.com>
2026-02-11 21:41:33 +0530

    address model created successfully but need to update something

--- addresses/models.py
+++ addresses/models.py
@@ -1,43 +1,26 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 
 class Address(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="addresses"
-    )
-
-    full_name = models.CharField(max_length=150)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="addresses")
+    full_name = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
-
-    address_line_1 = models.CharField(max_length=255)
-    address_line_2 = models.CharField(max_length=255, blank=True)
-
+    street = models.CharField(max_length=255)
     city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100, blank=True)
-    postal_code = models.CharField(max_length=20)
+    state = models.CharField(max_length=100)
+    postal_cpde = models.CharField(max_length=20)
     country = models.CharField(max_length=100)
-
     is_default = models.BooleanField(default=False)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ["-is_default", "-created_at"]
+    address_type = models.CharField(
+        max_length=10,
+        choices=[("shipping", "Shipping"), ("billing", "Billing")],
+        default="shipping"
+    )
 
     def __str__(self):
         return f"{self.full_name} - {self.city}, {self.country}"
     
-    def save(self, *args, **kwargs):
-        """
-        One user have only one address
-        """
-        if self.is_default:
-            Address.objects.filter(
-                user=self.user,
-                is_default=True
-            ).update(is_default=False)
-
-        super().save(*args, **kwargs)
+    def get_absolute_url(self):
+        return reverse("order_detail", kwargs={"pk": self.pk})
+    
