a9e602572c04f70455b7b00ce5c04ddb462971c5
Musthak <musthakcool55@gmail.com>
2026-02-12 06:14:11 +0530

    profile apps, view.py updated with profile_edit.html created

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,7 @@
 from django.views.generic import DetailView
+from django.views.generic.edit import UpdateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Profile
 
 class ProfileDetailView(DetailView):
@@ -7,3 +10,15 @@ class ProfileDetailView(DetailView):
     context_object_name = "profile"
     slug_field = "slug"
     slug_url_kwarg = "slug"
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = Profile
+    template_name = "profile_edit.html"
+    context_object_name = "profile"
+    fields = ["bio", "profile_pic", "date_of_birth", "gender"]
+    
+    def get_object(self):
+        return self.request.user.profile
+    
+    def get_success_url(self):
+        return reverse_lazy("profile_detail", kwargs={"slug": self.object.slug})
