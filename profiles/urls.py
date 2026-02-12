48bd5d5f36874c735b6e5071264c739c08a3ad76
Musthak <musthakcool55@gmail.com>
2026-02-12 06:16:16 +0530

    profileupdateview register just now on urls.py

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProfileDetailView
+from .views import ProfileDetailView, ProfileUpdateView
 
 urlpatterns = [
     path("<slug:slug>/", ProfileDetailView.as_view(), name="profile_detail"),
+    path("<slug:slug>/edit", ProfileUpdateView.as_view(), name="profile_edit"),
 ]
