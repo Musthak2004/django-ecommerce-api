932eae837c84d33abba0d1e2d82f717398929ef5
Musthak <musthakcool55@gmail.com>
2026-02-12 06:25:37 +0530

    profile_edit.html view, urls ellame correct aa setup panniyachu next profile_edit.html mattum konjam ui add pannanum

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ProfileDetailView, ProfileUpdateView
 
 urlpatterns = [
+    path("edit/", ProfileUpdateView.as_view(), name="profile_edit"),
     path("<slug:slug>/", ProfileDetailView.as_view(), name="profile_detail"),
-    path("<slug:slug>/edit", ProfileUpdateView.as_view(), name="profile_edit"),
 ]
